abb31798f6a82aa9926df813b4577cf933ce147b
Eric Murimi Njue <erick.murimi@obulexsolutions.com>
2016-10-31 11:30:32 +0300

    sdfjl

--- src/payment_agent_module/bills.php
+++ src/payment_agent_module/bills.php
@@ -1,24 +1,5 @@
 <?php
 if(App::isAjaxRequest()){
-    // initiate datatables
-    /*
- * DataTables example server-side processing script.
- *
- * Please note that this script is intentionally extremely simply to show how
- * server-side processing can be implemented, and probably shouldn't be used as
- * the basis for a large complex system. It is suitable for simple use cases as
- * for learning.
- *
- * See http://datatables.net/usage/server-side for full details on the server-
- * side processing requirements of DataTables.
- *
- * @license MIT - http://datatables.net/license_mit
- */
-
-    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-     * Easy set variables
-     */
-
 // DB table to use
     $table = 'customer_bills_view';
 
